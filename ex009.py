482a5da60481cb73b59c9eb9cb3978d118696425
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:08:11 -0300

    Create ex009.py

---
+++ ex009.py
@@ -0,0 +1,3 @@
+t = int(input('Tabuada do: '))
+print(f'{t} X 1 = {t*1}\n{t} X 2 = {t*2}\n{t} X 3 = {t *3}\n{t} X 4 = {t*4}\n{t} X 5 = {t*5}')
+print(f'{t} X 6 = {t*6}\n{t} X 7 = {t*7}\n{t} X 8 = {t *8}\n{t} X 9 = {t*9}\n{t} X 10 = {t*10}')
