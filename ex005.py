1f36c190ffd52de452a856e5e44d7189929eb677
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:06:20 -0300

    Create ex005.py

---
+++ ex005.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número: '))
+print(f'Analisando o número {n}, seu antecessor é o número {n-1} e seu sucessor é o número {n+1}.')
+# print('Analisando o número {}, seu antecessor é {} e seu sucessor é {}.'.format((n), (n-1), (n+1)))
