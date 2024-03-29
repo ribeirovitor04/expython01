3a20d4283d1268194aea5f33bcbebd5f51e24b14
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:21:58 -0300

    Create ex302.py

---
+++ ex302.py
@@ -0,0 +1,16 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um número: '))
+n3 = float(input('Digite um número: '))
+if n1 > n2 > n3:
+    print(f'{n1} é o maior e {n3} é o menor!')
+elif n1 > n3 > n2:
+    print(f'{n1} é o maior e {n2} é o menor!')
+elif n2 > n1 > n3:
+    print(f'{n2} é o maior e {n3} é o menor!')
+elif n2 > n3 > n1:
+    print(f'{n2} é o maior e {n1} é o menor!')
+elif n3 > n1 > n2:
+    print(f'{n3} é o maior e {n2} é o menor!')
+elif n3 > n2 > n1:
+    print(f'{n3} é o maior e {n1} é o menor!')
+print('--- FIM ---')
