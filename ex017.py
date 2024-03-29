13686becee20bdeccdb9860e11c9a32985f90187
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:13:41 -0300

    Create ex017.py

---
+++ ex017.py
@@ -0,0 +1,16 @@
+import math
+ca = float(input('Cateto adjascente: '))
+co = float(input('Cateto oposto: '))
+hip = math.hypot(ca,co)
+print(f'Hipotenusa: {hip}')
+
+# from math import sqrt
+# ca = float(input('Cateto adjascente: '))
+# co = float(input('Cateto oposto: '))
+# hip = sqrt(ca**2+co**2)
+# print(f'Hipotenusa: {hip}')
+#----------------------------------------------------#
+# ca = float(input('Cateto adjascente: '))
+# co = float(input('Cateto oposto: '))
+# hip = (ca**2+co**2)**1/2
+# print(f'Hipotenusa: {hip}')
