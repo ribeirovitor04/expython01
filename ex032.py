9bccd852cb1010e3f496611f7a952c037a07809d
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:57:47 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -11,6 +11,6 @@ elif n2 > n3 > n1:
     print(f'{n2} é o maior e {n1} é o menor!')
 elif n3 > n1 > n2:
     print(f'{n3} é o maior e {n2} é o menor!')
-elif n3 > n2 > n1:
+else:
     print(f'{n3} é o maior e {n1} é o menor!')
 print('--- FIM ---')
