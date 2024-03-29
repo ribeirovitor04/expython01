cd9b4fb9d1eb9188e50b54424a1a66b4b45483ba
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:13:16 -0300

    Create ex016.py

---
+++ ex016.py
@@ -0,0 +1,12 @@
+from math import trunc
+n = float(input('Digite um valor: '))
+inteiro = trunc(n)
+print(f'O valor digitado é {n} e a sua porção inteira é igual a {inteiro}.')
+
+# import math
+# n = float(input('Digite um valor: '))
+# inteiro = math.trunc(n)
+# print(f'O valor digitado é {n} e a sua porção inteira é igual a {inteiro}.')
+# -----------------------------------------------------------------------------------------#
+# n = float(input('Digite um valor: '))
+# print(f'O valor digitado é {n} e a sua porção inteira é igual a {int(inteiro)}.')
