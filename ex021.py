8c4567d09c014a0ad3c4adfa3d12697bc2f85d82
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-03-29 11:16:29 -0300

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,1 +1,14 @@
+nome = input('Digite seu nome completo: ')
+a = nome.split()
+b = ''.join(a)
+print(f'Seu nome em maiúsculas é {nome.upper()}')
+print(f'Seu nome em minúsculas é {nome.casefold()}')
+# Ou no lugar de CASEFOLD poderia ser colocado LOWER
+print(f'Seu nome completo tem {len(b)} letras.')
+print(f'Seu primeiro nome é {a[0]} e ela tem {len(a[0])} letras.')
 
+#nome = str(input('Digite seu nome completo: ')).strip()
+#print(f'Seu nome em maiúsculas é {nome.upper()}')
+#print(f'Seu nome em minúsculas é {nome.casefold()}')
+#print(f'Seu nome completo tem {len(nome)-nome.count(' ')} letras.')
+#print(f'Seu primeiro nome tem {nome.find(' ')} letras.')
